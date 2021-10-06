2d03ea5e3008a20acebb7e843c70f06a8fad5d86
Nasa28 <kaluchinasaagu@gmail.com>
2021-10-06 17:02:16 +0100

    Set propType to string

--- src/component/ButtonPanel.js
+++ src/component/ButtonPanel.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Button from './Button';
 const ButtonPanel = () => {
   return (
     <div>
-      <h1>Get home</h1>
+      <h1>ButtonPanel</h1>
+    <Button/>
     </div>
   );
 };
--- src/component/Display.js
+++ src/component/Display.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const Display = () => {
+const Display = (props) => {
+const {result} = props
   return (
     <div>
-      <h1>This is good</h1>
+      <h1>Display</h1>
     </div>
   );
 };
 
+Display.prototype = {
+  result: PropTypes.string.isRequired
+}
+Display.defaultProps = {
+  result: 0
+}
 export default Display;
