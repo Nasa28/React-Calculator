ae30422fabc7157b983403601436c06e9c1a786f
Nasa28 <kaluchinasaagu@gmail.com>
2021-10-10 23:21:13 +0100

    Complete operate function

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,8 +1,27 @@
 import Big from 'big.js';
-import React from 'react'
 
-const operate = () => {
+const operate = (numberOne, numberTwo, operation) => {
+  const num1 = Big(numberOne);
+  const num2 = Big(numberTwo);
+  let result;
 
-}
+  if (operation === '+') {
+    result = num1.plus(num2);
+  }
+  if (operation === '-') {
+    result = num1.minus(num2);
 
-export default operate
+    if (operation === 'x') {
+      result = num1.times(num2);
+    }
+    if (operation === '÷') {
+      result = num1.div(num1);
+    }
+    if (operation === '%') {
+      result = one.div(Big('100'));
+    }
+  }
+  return result.toPrecision();
+};
+
+export default operate;
