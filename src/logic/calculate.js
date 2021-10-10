45088725970b030c3793a283799fa5ca758c2b08
Nasa28 <kaluchinasaagu@gmail.com>
2021-10-11 00:01:05 +0100

    Add calculate logic

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -3,7 +3,7 @@ import operate from './operate';
 const calculate = (data, buttonName) => {
   const { total, next, operation } = data;
 
-  let operators = ['÷', 'X', '+', '-'];
+  let operator = ['÷', 'X', '+', '-'];
   let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   if (buttonName === 'AC') {
@@ -27,62 +27,52 @@ const calculate = (data, buttonName) => {
       total /= 100;
     }
   }
-  /*
-  3. If buttonName = %
 
- if next, next/=100, else total/=100
- 
- 4 if buttonName = "."
- (a)if no total , total+= "0."
- (b) if next does not include "." and operation, next+= "."
- (c) if no next and operation, next += "0."
- (d) if total does not include "." and no operation total += '.';
-
- (5) if operator.includes(buttonName) && total = null, total = 0
-
- if next && operator.includes(buttonname)
-
- if (operator.includes(buttonName)) {
-    if (total && !next) {
-      operation = btnName;
+  if (buttonName === '.') {
+    if (!total) {
+      total += '0.';
+    } else if (!next.includes(buttonName) && operation) {
+      next += '.';
+    } else if (!total.includes('.') && !operation) {
+      total += '.';
+    } else if (!next && operation) {
+      next += '0.';
     }
-    if (total && next && operation) {
+  }
+
+  if (operator.includes(buttonName)) {
+    if (!total) {
+      total = null;
+    } else if (!next && total) {
+      operation = buttonName;
+    } else if (total && next && operation) {
       total = operate(total, next, operation);
-      next = '';
+      next = null;
       operation = buttonName;
     }
     return data;
   }
 
-
-   if (buttonName === '=') {
-    if (total && !next) {
-      total = total;
-    }
-    if (total && next && operation) {
-      total = operate(total, next, operation);
-      next = '';
-      operation = '';
-    }
-
-    return data;
-  }
-  return data;
-};
-
-
-
-if (operator.includes(buttonname))
- if (!total || (total && !operation)) {
-      data.total += btnName;
-    }
-
+  if (numbers.includes(buttonName)) {
     if (total && operation) {
-      data.next += btnName;
+      next += buttonName;
+    } else if (total && !operation) {
+      total += buttonName;
     }
     return data;
   }
- */
+
+  if (buttonName === '=') {
+    if (!next && total) {
+      total = total;
+    } else if (total && next && operation) {
+      total = operate(total, next, operation);
+      next = null;
+      operation = null;
+    }
+    return data;
+  }
+
   return data;
 };
 
