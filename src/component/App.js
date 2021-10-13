d82c358e89e72f57778b481aaaa41b5b1d4cfcd4
Nasa28 <kaluchinasaagu@gmail.com>
2021-10-13 23:33:16 +0100

    Implement handleClick method

--- src/component/App.js
+++ src/component/App.js
@@ -5,7 +5,7 @@ import ButtonPanel from './ButtonPanel';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -13,7 +13,14 @@ class App extends React.Component {
       next: null,
       operation: null,
     };
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick = (buttonName) => {
+    const { data } = this.state;
+    this.setState(calculate(data, buttonName));
+  };
+
   render() {
     return (
       <>
