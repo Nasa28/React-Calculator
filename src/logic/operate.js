6cc97574bc282d30d8051d37012dc5ccb7690aa6
Nasa28 <kaluchinasaagu@gmail.com>
2021-10-25 13:19:39 +0100

    Update operate.js file

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -11,13 +11,14 @@ const operate = (numberOne, numberTwo, operation) => {
     result = num1.minus(num2);
   } else if (operation === 'x') {
     result = num1.times(num2);
-  } else if (operation === '÷') {
-    result = num1.div(num2);
   } else if (operation === '%') {
     result = num1.div(Big('100'));
   }
+  if (operation === '÷' && num2.valueOf() !== '0') {
+    result = num1.div(num2);
+  } else result = "Can't divide by zero";
 
-  return result.toPrecision();
+  return result.toString();
 };
 
 export default operate;
